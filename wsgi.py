7c28e493774482cd439ed31167eabfe7e52a291e
yujinphj-gif <yujinphj@gmail.com>
2026-07-16 16:19:01 +0900

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,6 @@
-"""
-Flask 앱의 엔트리 포인트 커스텀 예제
-필요에 따라 수정하여 사용할 수 있습니다.
-"""
-
-# app.py 대신 wsgi.py 사용하는 경우를 위한 래퍼
+import os
 from app import app, socketio
 
 if __name__ == '__main__':
-    # 프로덕션 환경
-    # gunicorn --worker-class eventlet -w 1 wsgi:app
-    socketio.run(
-        app,
-        host='0.0.0.0',
-        port=5000,
-        debug=False,
-        use_reloader=False
-    )
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
